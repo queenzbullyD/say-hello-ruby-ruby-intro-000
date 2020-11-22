f12d51a4eca2b269d14b599e5b73a44e747afb70
darren a vinson jr <darrenvinson@gmail.com>
2020-11-22 12:35:35 +0000

    Automatically backed up by Learn

---
+++ spec/say_hello.rb
@@ -0,0 +1,9 @@
+def say_hello
+  say_hello('Kent Beck!')
+end
+
+
+def say_hello1
+  puts 'Hello Ruby Programmer!'
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require_relative '../say_hello'
 
 RSpec.configure do |config|
-  # config here
+  def say_hello
+    say_hello('Kent Beck!')
+  end
+
+
+  def say_hello
+    puts 'Hello Ruby Programmer!'
+  
 end
